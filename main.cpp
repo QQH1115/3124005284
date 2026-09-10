e8d0fb257bed930c1f0a9d37500dcc56782935c4
QQH1115 <3764232401@qq.com>
2026-09-10 21:18:22 +0800

    0.1输入与分词部分

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[]) {
+    cout<<argv[1]<<" "<<argv[2]<<" "<<argv[3]<<endl;
     return 0;
 }
